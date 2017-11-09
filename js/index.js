3eb13cc8773e2330e4e56f73e9f2aa3db941af9c
Faaa97 <faaa97@hotmail.com>
2017-11-09 09:44:07 +0000

    Reparamos las expresiones regulares

--- js/index.js
+++ js/index.js
@@ -3,15 +3,15 @@ function validacion() {
     var apellidos = document.forms["suscripcion"]["apellidosf"].value;
     var email = document.forms["suscripcion"]["emailf"].value;
   
-    if (!RegExp("[a-zA-Z]+").test(nombre)) {
+    if (!RegExp("[a-zA-Z ]+").test(nombre)) {
         alert("Introduce un nombre válido");
         return false;
     }
-    if (!RegExp("[a-zA-Z]+").test(apellidos)) {
+    if (!RegExp("[a-zA-Z ]+").test(apellidos)) {
         alert("Introduce unos apellidos válidos");
         return false;
     }
-    if(!RegExp("[a-zA-Z0-9]+@[a-zA-Z]+\\.[a-zA-Z]+").test(email)){
+    if(!RegExp("[a-zA-Z0-9\\.]{2,}@[a-zA-Z]{3,}\\.[a-zA-Z]{2,}").test(email)){
         alert("Introduce un correo electrónico válido");
           return false;
     }
